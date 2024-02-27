97de7bb991d09329a977029ed68b3390b9145be1
wynehtetaung <wynehtet2022@gmail.com>
2024-02-27 22:43:53 +0630

    business router

--- routes/business.js
+++ routes/business.js
@@ -5,28 +5,18 @@ router.post("/unit", (req, res) => {
   let { unit } = req.body;
   let totalUnits = unit;
   let currentUnit = 0;
-  let perUnits = [];
-  let Id = [];
   let calculateKyats = [];
-  let ranges = [];
   let totalData = [];
   let totalKyats = 0;
   const calculateFun = (unit, perUnit, range) => {
     const calculateKyat = unit * perUnit;
     calculateKyats = [...calculateKyats, calculateKyat];
-    totalData = [...totalData, { range, perUnit, calculateKyat }];
-    perUnits = [...perUnits, perUnit];
-    const id = Id.length + 1;
-    Id = [...Id, id];
-    ranges = [...ranges, range];
+    const id = totalData.length + 1;
+    totalData = [...totalData, { id, range, perUnit, calculateKyat }];
   };
   const sendHomeMeterBills = () => {
     totalKyats = calculateKyats.reduce((a, b) => a + b);
     res.status(202).json({
-      Id,
-      calculateKyats,
-      perUnits,
-      ranges,
       totalKyats,
       totalUnits,
       totalData,
@@ -78,17 +68,12 @@ router.post("/unit", (req, res) => {
   } else {
     const perUnit = 125;
     const calculateKyat = unit * perUnit;
-    perUnits = [perUnit];
     calculateKyats = [calculateKyat];
     const range = ["1-500"];
-    Id = [1];
+    const id = [1];
     totalKyats = calculateKyat;
-    totalData = [...totalData, { range, perUnit, calculateKyat }];
+    totalData = [...totalData, { id, range, perUnit, calculateKyat }];
     res.status(202).json({
-      Id,
-      calculateKyats,
-      perUnits,
-      ranges,
       totalKyats,
       totalUnits,
       totalData,
@@ -97,68 +82,74 @@ router.post("/unit", (req, res) => {
 });
 
 router.post("/kyat", (req, res) => {
-  let { kyats } = req.body;
+  let kyat = req.body.unit;
   let currentKyat = 0;
-  let perUnits = [];
   let calculateUnits = [];
+  let totalKyats = kyat;
   let totalUnits = 0;
-  const calculateFun = (kyats, perUnit) => {
-    const calculateKyat = kyats / perUnit;
-    calculateUnits = [...calculateUnits, calculateKyat];
-    perUnits = [...perUnits, perUnit];
+  let totalData = [];
+  const calculateFun = (kyat, perUnit, range) => {
+    const calculateUnit = kyat / perUnit;
+    const id = totalData.length + 1;
+    calculateUnits = [...calculateUnits, calculateUnit];
+    totalData = [...totalData, { id, range, kyat, perUnit }];
   };
   const sendHomeMeterBills = () => {
     totalUnits = calculateUnits.reduce((a, b) => a + b);
-    res.status(202).json({ calculateUnits, totalUnits, perUnits });
+    res.status(202).json({ totalKyats, totalUnits, totalData });
   };
 
-  if (kyats > 62500) {
-    currentKyat = kyats - 62500;
-    calculateFun(62500, 125);
+  if (kyat > 62500) {
+    currentKyat = kyat - 62500;
+    calculateFun(62500, 125, "1-500");
     if (currentKyat > 607500) {
       currentKyat -= 607500;
-      calculateFun(607500, 135);
+      calculateFun(607500, 135, "501-5000");
       if (currentKyat > 725000) {
         currentKyat -= 725000;
-        calculateFun(725000, 145);
+        calculateFun(725000, 145, "5001-10000");
         if (currentKyat > 1550000) {
           currentKyat -= 1550000;
-          calculateFun(1550000, 155);
+          calculateFun(1550000, 155, "10001-20000");
           if (currentKyat > 4950000) {
             currentKyat -= 4950000;
-            calculateFun(4950000, 165);
+            calculateFun(4950000, 165, "20001-50000");
             if (currentKyat > 8750000) {
               currentKyat -= 8750000;
-              calculateFun(8750000, 175);
+              calculateFun(8750000, 175, "50001-100000");
               if (currentKyat > 0) {
-                calculateFun(currentKyat, 180);
+                calculateFun(currentKyat, 180, "100001 and above");
                 sendHomeMeterBills();
               }
             } else {
-              calculateFun(currentKyat, 175);
+              calculateFun(currentKyat, 175, "50001-100000");
               sendHomeMeterBills();
             }
           } else {
-            calculateFun(currentKyat, 165);
+            calculateFun(currentKyat, 165, "20001-50000");
             sendHomeMeterBills();
           }
         } else {
-          calculateFun(currentKyat, 155);
+          calculateFun(currentKyat, 155, "10001-20000");
           sendHomeMeterBills();
         }
       } else {
-        calculateFun(currentKyat, 145);
+        calculateFun(currentKyat, 145, "5001-10000");
         sendHomeMeterBills();
       }
     } else {
-      calculateFun(currentKyat, 135);
+      calculateFun(currentKyat, 135, "501-5000");
       sendHomeMeterBills();
     }
   } else {
     const perUnit = 125;
-    const calculateKyat = kyats / perUnit;
-    totalUnits = calculateKyat;
-    res.status(202).json({ calculateKyat, totalUnits, perUnit });
+    const totalKyats = kyat;
+    const calculateUnit = kyat / perUnit;
+    totalUnits = calculateUnit;
+    const range = "1-500";
+    const id = 1;
+    totalData = [...totalData, { id, range, kyat, perUnit }];
+    res.status(202).json({ totalData, totalKyats, totalUnits });
   }
 });
 
